bb6e3963cb4a312fd2301f56b3c2da45bcd462fc
MaksimKu <kuzminowmax@gmail.com>
2023-11-06 23:32:27 +0300

    fix:index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./main.scss";
 
-import App from './components/app/app';
+// import App from './components/app/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let ji = {
+    first: "max",
+    tu: "mix"
+}
+
+
+function fun (i){
+    return i.first + i.tu
+}
+let i = <div className='tafel'></div>
+
+ReactDOM.render(i, document.getElementById('root'));
